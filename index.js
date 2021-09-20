b1ee95e5c51b0e6f6f9b76e0135c88854e69d279
anaikarsaad <54991344+anaikarsaad@users.noreply.github.com>
2021-09-20 16:03:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+//get the form by its id
+const form = document.getElementById("contact-form"); 
+
+//1.
+const formEvent = form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  //2.
+  let mail = new FormData(form);
+
+  //3.
+  sendMail(mail);
+})
+const sendMail = (mail) => {
+    //1.
+    fetch("http://127.0.0.1:5500/send", {
+      method: "post", //2.
+      body: mail, //3.
+  
+    }).then((response) => {
+      return response.json();
+    });
+  };
